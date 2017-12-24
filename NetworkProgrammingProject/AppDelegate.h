0744bbe7bcb0a9225842b4546b4f6f5652aacbe8
Rex <rex45678@gmail.com>
2017-12-24 11:14:36 +0800

    Initial Commit

---
+++ NetworkProgrammingProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetworkProgrammingProject
+//
+//  Created by Rex on 2017/12/24.
+//  Copyright © 2017年 Rex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
