8d3cdb6b842d2bd2ffd21dca32076b37fe35a4af
Rex <rex45678@gmail.com>
2018-01-04 06:22:39 +0800

    新增readme

--- NetworkProgrammingProject/Network/LightNetworkManager.m
+++ NetworkProgrammingProject/Network/LightNetworkManager.m
@@ -49,12 +49,18 @@
 
 -(void)loadDataWithBlock:(void (^)(NPPLampObject * _Nonnull))block withCancelBlock:(void (^)(NSError * _Nonnull))cancelBlock
 {
-    self.loading = YES;
-    __weak typeof(self) weakself = self;
+    self.loading = YES; //載入中
+    __weak typeof(self) weakself = self;    //避免memory leek
+    // 對 firebase 操作取得 database 資料
+    // self.refLamp 為 [[[FIRDatabase database] reference] child:@"lamp"]] 表示取得lamp這個key的資料
     [self.refLamp observeEventType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
         
-        weakself.loading = NO;
+        weakself.loading = NO;  //載入完成
+        // snapshot.value 為lamp的值，資料格式為 dictionary
+        // NPPLampObject 為物件，objectFromDictionary 可將 dictionary 轉為物件
         NPPLampObject *lamp = [NPPLampObject objectFromDictionary:snapshot.value];
+        
+        // 將物件callback回去給呼叫者操作
         if (block) {
             block(lamp);
         }
@@ -69,20 +75,17 @@
 
 -(void)setCheck:(BOOL)check
 {
-//    self.loading = YES;
     NSNumber *checkNumber = check?@YES:@NO;
     [[self.refLamp child:@"check"] setValue:checkNumber];
 }
 
 -(void)setColor:(float)color
 {
-//    self.loading = YES;
     [[self.refLamp child:@"color"] setValue:@(color)];
 }
 
 -(void)setIntensity:(float)intensity
 {
-//    self.loading = YES;
     [[self.refLamp child:@"intensity"] setValue:@(intensity)];
 }
 
@@ -90,10 +93,13 @@
 {
     self.loading = YES;
     __weak typeof(self) weakself = self;
+    // 對 firebase 操作取得 database 資料
+    // self.refSensor 為 [[[[FIRDatabase database] reference] child:@"sensor"] child:@"temp"]] 表示取得sensor.temp 這個key的資料
     [self.refSensor observeEventType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
         
         weakself.loading = NO;
         NSDictionary *dict = snapshot.value;
+        // 避免資料格式不是 NSDictionary
         if (!dict || ![dict isKindOfClass:[NSDictionary class]]) {
             if (cancelBlock) {
                 NSError *error = [NSError errorWithDomain:@"com.htw.firebase" code:-1 userInfo:@{@"description":@"return null"}];
@@ -101,14 +107,18 @@
             }
             return ;
         }
+        // 將Dictionary的所有值轉換為物件陣列
         NSArray *arrayTemp = [NPPSensorObject objectFromArray:dict.allValues];
         
+        // 依時間排序
         NSArray *sortedArray;
         sortedArray = [arrayTemp sortedArrayUsingComparator:^NSComparisonResult(NPPSensorObject *a, NPPSensorObject *b) {
             NSDate *first = a.time;
             NSDate *second = b.time;
             return [first compare:second];
         }];
+        
+        // 將物件陣列callback回去給呼叫者操作
         if (block) {
             block(sortedArray);
         }
---
+++ readme.md
@@ -0,0 +1,14 @@
+#安裝方式
+##環境
+* mac OS
+* Xcode 9
+
+##安裝cocoapods
+```
+# Xcode 8 + 9
+$ sudo gem install cocoapods
+```
+##安裝套件
+```
+$ pod install
+```
